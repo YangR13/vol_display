a89e7d004838d96e9b375abd568277668d3294b9
killer225whale <killer225whale@gmail.com>
2014-04-21 12:34:53 -0700

    Added more to basic initialization
    Filled out some stuff

--- vol_display.ino
+++ vol_display.ino
@@ -1,4 +1,29 @@
-void setup() {
+#include <Servo.h>
+
+Servo esc;
+
+//Define pins;
+int led_Data = A0;     //A# (0-5) = Analog pins
+int led_Latch = A1;
+int led_Clock = A2;
+int _RPM = A5;
+int esc_Out = 9;
+
+//Define variables;
+int esc_Arm = 10;
+
+void setup()
+{
+  //Configure IO Pins
+  pinMode(led_Data, OUTPUT);
+  pinMode(led_Latch, OUTPUT);
+  pinMode(led_Clock, OUTPUT);
+  pinMode(_RPM, INPUT);
+  esc.attach(esc_Out);
+  esc.write(esc_Arm);
+  
+  
+  
   int maxTicks = 1000;
   int tick = 0;
   
@@ -9,7 +34,8 @@ void setup() {
   //fill array with proper values
 }
 
-void loop() {
+void loop()
+{
   
   
   
