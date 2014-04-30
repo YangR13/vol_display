ab8d058f810328ceb4ca0e42828361d073cb6f63
killer225whale <killer225whale@gmail.com>
2014-04-30 13:17:54 -0700

    Added more to startup

--- vol_display.ino
+++ vol_display.ino
@@ -1,26 +1,30 @@
 #include <Servo.h>
 
-Servo esc;
+Servo ESC;
 
 //Define pins;
-int led_Data = A0;          //A# (0-5) = Analog pins
-int led_Latch = A1;
-int led_Clock = A2;
-int RPM_check = A5;
-int esc_Out = 9;
+int led_Data = A1;          //A# (0-5) = Analog pins
+int led_Latch = A2;
+int led_Clock = A3;
+int RPM_check = A0;
+int ESC_Out = 11;
 
 
 //Define variables;
-int esc_Arm = 10;
-int HDD_lowbound = 75;
-int HDD_highbound = 165;
-int RPM_target = 1000;      //Target RPM
+int ESC_Arm = 10;
+int ESC_lowbound = 80;
+//int ESC_highbound = 165;
+//int RPM_target = 1000;      //Target RPM
+int ESC_speed = 100;
+int delay_iniESC = 2000;
+
+//Helper setting variables;
+int delay_overhead;
+int delay_degree;
 
 int vol_slices = 360;       //# of separate radial slices that will be plexed through;
 int vol_layers = 16;        //# of vertical layers;
 int vol_shiftsperlayer = 4; //# of 8-bit shift registers per layer;
-int vol_totalbytes = vol_shiftsperlayer*vol_layers;
-
 
 void setup()
 {
@@ -31,10 +35,17 @@ void setup()
   pinMode(RPM_check, INPUT);
   
   //Motor start and ramp up;
-  esc.attach(esc_Out);
-  esc.write(esc_Arm);
-  delay(2000);
+  esc.attach(ESC_Out);
+  esc.write(ESC_Arm);
+  delay(5000);
+  //Slowly ramp up speed;
+  for(i = ESC_lowbound; i <= ESC_highbound && i <= target; i++)
+  {
+    esc.write(i);
+    delay(delay_iniESC);
+  }
   
+  //Read overhead delay time;
   
   
   int maxTicks = 1000;
